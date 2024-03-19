3f7a15edebc8301ac57c96378c4b65569981ac47
Chit Khonn Cho <chitkhonncho@Chits-Laptop.local>
2024-03-19 12:19:20 +0700

    create login and sign up form

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="welcome container">
-    <p>Welcome</p>
+
+    <div v-if="showLoginForm">
+
+      <Login></Login>
+      <p>Not a member? <span @click="showLoginForm=!showLoginForm">Create account</span></p>
+
+    </div>
+    <div v-else>
+
+      <SignUp></SignUp>
+      <p>Already member? <span @click="showLoginForm=!showLoginForm" >Login account?</span></p>
+
+    </div>
+
+
   </div>
 </template>
 
 <script>
+import SignUp from "@/views/SignUp.vue";
+import Login from "@/views/Login.vue";
+import {ref} from "vue";
+
 export  default  {
+  components: {Login, SignUp},
+
+  setup(){
+    let showLoginForm = ref(true);
+
+    return {showLoginForm};
+  }
 
 }
 </script>
 
 <style>
-  .welcome{
-    text-align: center;
-    padding: 20px 0;
-  }
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+/* form styles */
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+/* .welcome label {
+  display: block;
+  margin: 20px 0 10px;
+} */
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span{
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
+}
 </style>
